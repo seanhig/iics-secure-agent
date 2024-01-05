29a04a3b253e46bc9513f1eb209757bc41f26ec1
Sean Hignett <sean@idstudios.io>
2024-01-05 10:33:13 -0700

    add volume mounts

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 docker run --name iics-secure-agent \
   --env-file .env \
   -h iics-docker-agent \
+  -v iics-agent-volume:/home/iics-agent \
   -v ./flatfiles:/flatfiles \
   -d local/iics-secure-agent:latest
